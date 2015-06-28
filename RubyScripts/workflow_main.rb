e4b4efaa6af44e0d471eb13c172c711db5a59ee9
Piso Mojado Grande <becky_weiss@hotmail.com>
2015-06-27 22:18:03 -0400

    Long poll for decision task

--- RubyScripts/workflow_main.rb
+++ RubyScripts/workflow_main.rb
@@ -3,6 +3,8 @@ require 'aws-sdk' # gem install aws-sdk
 
 DOMAIN_NAME = 'CondorcetVote'
 WORKFLOW_TYPE_NAME = 'TestWorkflowType'
+TASK_LIST_NAME = 'TestTaskList'
+DECIDER_IDENTITY = 'TestDecider'
 
 access_key_id = ENV['AWS_ACCESS_KEY']
 secret_key = ENV['AWS_SECRET_KEY']
@@ -20,3 +22,11 @@ workflow_type = swf.list_workflow_types(
   :registration_status => 'REGISTERED'
 )
 puts workflow_type.inspect
+
+puts "Starting poll for #{TASK_LIST_NAME}"
+decision_task = swf.poll_for_decision_task(
+  :domain => domain.name,
+  :task_list => { :name => TASK_LIST_NAME },
+  :identity => DECIDER_IDENTITY
+)
+puts decision_task.inspect
